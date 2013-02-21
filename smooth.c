62d94e14e9d0669caa4eeec70af624de7da3d9d6
Arjun Saini <Arjun@mtw-10-164-205-136.mtw.jhu.edu>
2013-02-20 19:34:07 -0500

    gasdg

--- smooth.c
+++ smooth.c
@@ -73,7 +73,7 @@ int main()
   }
 
   //printf ("Serial YX smoother took %d seconds and %d microseconds\n",s,u );
-  printf ("Serial YX smoother took %d seconds and %d microseconds\n", (s+(u/1000000)) );
+  printf ("Serial YX smoother took %d seconds\n", (s+(u/1000000)) );
 
   /* get initial time */
   gettimeofday ( &ta, NULL );
